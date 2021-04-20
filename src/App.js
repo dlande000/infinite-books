96770a40b8415d68a02e65325bcabce9bacb94a8
David Anderson <dlande000@gmail.com>
2021-04-19 21:02:48 -0400

    Break search component into separate directory

--- src/App.js
+++ src/App.js
@@ -1,45 +1,12 @@
-import React, { useState, useRef, useCallback } from 'react';
-import useBookSearch from './useBookSearch';
+import React from 'react';
+
+import Search from './search/Search';
 
 const App = () => {
-  const [query, setQuery] = useState('');
-  const [pageNum, setPageNum] = useState(1);
-
-  const {
-    books,
-    isLoading,
-    hasMoreResults,
-    hasError,
-  } = useBookSearch(query, pageNum);
-
-  const observer = useRef();
-  const lastBookElementRef = useCallback(node => {
-    if (isLoading) return;
-    if (observer.current) observer.current.disconnect();
-    observer.current = new IntersectionObserver(entries => {
-      if (entries[0].isIntersecting && hasMoreResults) {
-        setPageNum(prevPageNum => ++prevPageNum);
-      }
-    });
-    if (node) observer.current.observe(node);
-    console.log(node);
-  }, [isLoading, hasMoreResults]);
-
-  const handleSearch = e => {
-    e.preventDefault();
-    setQuery(e.target.value);
-    setPageNum(1);
-  };
-
   return (
     <>
-      <input type="text" value={query} onChange={handleSearch}/>
-      {books.map((book, i) => (
-        <div key={book.title + i} ref={i === books.length - 1 ? lastBookElementRef : null}>{book.title}</div>
-      ))}
-      <div>{isLoading && 'Loading . . . '}</div>
-      <div>{hasError && 'Error . . . '}</div>
-      <div>{(query && !isLoading && !hasMoreResults) && 'That is all the books I could find! :)'}</div>
+      <div>This is the app header</div>
+      <Search/>
     </>
   );
 };
