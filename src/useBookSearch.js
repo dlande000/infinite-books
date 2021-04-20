6ddae218a904e16619101ae1426bbad1d164bb41
David Anderson <dlande000@gmail.com>
2021-04-19 20:29:08 -0400

    Add cancelToken field to Axios query

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -3,13 +3,20 @@ import axios from 'axios';
 
 const useBookSearch = (q, page) => {
   useEffect(() => {
+    let cancel;
+
     axios({
       method: 'GET',
       url: 'http://openlibrary.org/search.json',
       params: { q, page },
+      cancelToken: new axios.CancelToken(c => cancel = c),
     }).then(res => {
       console.log(res);
+    }).catch(e => {
+      if (axios.isCancel(e)) return;
     });
+
+    return () => cancel();
   }, [q, page]);
 
   return null;
