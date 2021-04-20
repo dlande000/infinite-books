a2d2823c96dbb754cea0637bb91b6583f052b2be
David Anderson <dlande000@gmail.com>
2021-04-19 20:19:21 -0400

    Add query and pageNum state to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useBookSearch from './useBookSearch';
 
 const App = () => {
+  const [query, setQuery] = useState('');
+  const [pageNum, setPageNum] = useState('');
+
+  const handleSearch = e => {
+    e.preventDefault();
+    setQuery(e.target.value);
+    setPageNum(1);
+  };
+
+  useBookSearch(query, pageNum);
 
   return (
     <>
-      <input type="text"></input>
+      <input type="text" onChange={handleSearch}></input>
       <div>example</div>
       <div>example</div>
       <div>example</div>
