c5d06270c04de4d69f1ab7c0cd9841d706a77e4f
David Anderson <dlande000@gmail.com>
2021-04-20 12:21:08 -0400

    Add comments from API for accessing cover images

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const App = () => {
       <h1>infinite books.</h1>
       <div>So much to read, so little time.</div>
       <div>This is the description for infinite books.</div>
-      <div>This is the description for infinite books.</div>
+      <div>This is the description for infinite books. https://openlibrary.org/developers/api</div>
       <ul>
-        <li><a href={'google.com'}>Portfolio</a></li>
-        <li><a href={'google.com'}>GitHub</a></li>
+        <li><a href={'http://davidanderson.nyc/'}>Portfolio</a></li>
+        <li><a href={'https://github.com/dlande000/infinite-books'}>GitHub</a></li>
       </ul>
       <Search/>
     </>
