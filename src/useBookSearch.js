b7342962b6d9612723972371e4547f4f79935b44
David Anderson <dlande000@gmail.com>
2021-04-19 20:38:54 -0400

    Add setting state logic for book data in useBookSearch hook

--- src/useBookSearch.js
+++ src/useBookSearch.js
@@ -2,7 +2,15 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const useBookSearch = (q, page) => {
+  const [books, setBooks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasMoreResults, setHasMoreResults] = useState(false);
+  const [hasError, setHasError] = useState(false);
+
   useEffect(() => {
+    setIsLoading(true);
+    setHasError(false);
+
     let cancel;
 
     axios({
@@ -11,15 +19,24 @@ const useBookSearch = (q, page) => {
       params: { q, page },
       cancelToken: new axios.CancelToken(c => cancel = c),
     }).then(res => {
-      console.log(res);
+      setBooks(prevBooks => [...prevBooks, ...res.data.docs]);
+      setHasMoreResults(res.data.docs.length > 0);
+      setIsLoading(false);
     }).catch(e => {
       if (axios.isCancel(e)) return;
+
+      setHasError(true);
     });
 
     return () => cancel();
   }, [q, page]);
 
-  return null;
+  return {
+    books,
+    isLoading,
+    hasMoreResults,
+    hasError,
+  };
 };
 
 export default useBookSearch;
