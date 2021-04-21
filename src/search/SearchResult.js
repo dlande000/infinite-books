759017e5efb42874877416afe065848da6bb0037
David Anderson <dlande000@gmail.com>
2021-04-21 16:46:04 -0400

    Compose logic for correctly displaying publishers and ISBNs

--- src/search/SearchResult.js
+++ src/search/SearchResult.js
@@ -1,32 +1,62 @@
 import React from 'react';
 
-const AUTHOR = 'Author:';
-const AUTHORS = 'Authors:';
-const NO_AUTHOR_PROVIDED = 'No author(s) provided.';
+const AUTHOR = 'Author: ';
+const AUTHORS = 'Authors: ';
+const NO_AUTHOR_PROVIDED = 'No author provided.';
+const PUBLISHER = 'Publisher: ';
+const PUBLISHERS = 'Publishers: ';
+const NO_PUBLISHER_PROVIDED = 'No publisher provided.';
+const ISBN = 'ISBN: ';
+const ISBNS = 'ISBNs: ';
+const NO_ISBN_PROVIDED = 'No ISBN provided.';
 
 const SearchResult = ({ book }) => {
+  let authors;
+  let publisher;
+  let isbn;
+
+  if (book.author_name) {
+    if (book.author_name.length > 1) {
+      authors = AUTHORS;
+    } else {
+      authors = AUTHOR;
+    }
+
+    authors += book.author_name.join(', ');
+  } else {
+    authors = NO_AUTHOR_PROVIDED;
+  }
+
+  if (book.publisher) {
+    if (book.publisher.length > 1) {
+      publisher = PUBLISHERS;
+    } else {
+      publisher = PUBLISHER;
+    }
+
+    publisher += book.publisher.join(', ');
+  } else {
+    publisher = NO_PUBLISHER_PROVIDED;
+  }
+
+  if (book.isbn) {
+    if (book.isbn.length > 1) {
+      isbn = ISBNS;
+    } else {
+      isbn = ISBN;
+    }
+
+    isbn += book.isbn.join(', ');
+  } else {
+    isbn = NO_ISBN_PROVIDED;
+  }
+
   return (
     <div className="box search-result">
-      {book.title}
-      <br/>
-      {book.author_name ? (
-        <>
-          <div>
-            {book.author_name.length > 1 ? AUTHORS : AUTHOR}
-          </div>
-          {book.author_name.map(author => (
-            <div>{author}</div>
-          ))}
-        </>
-        ) : NO_AUTHOR_PROVIDED
-      }
-      {/* {book.isbn && (book.author_name.map(author => (
-        <div>{author}</div>
-      )))}
-      {book.publisher && (book.author_name.map(author => (
-        <div>{author}</div>
-      )))} */}
-      <br/>
+      <div>{book.title}</div>
+      <div>{authors}</div>
+      <div>{publisher}</div>
+      <div>{isbn}</div>
     </div>
   )
 }
